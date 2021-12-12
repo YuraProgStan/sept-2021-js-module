6b38ea1cd62b2e33c8973129e5faff7927777fd5
YuraProgStan <yurstan@gmail.com>
2021-12-12 10:05:51 +0200

    Lesson12. all

--- Lesson12/Additional12/additional12.js
+++ Lesson12/Additional12/additional12.js
@@ -8,45 +8,36 @@ let divContainer = document.createElement('div');
 divContainer.classList.add('container');
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(arrayUsers=> {
+    .then(arrayUsers => {
         for (const arrayUser of arrayUsers) {
             let div = document.createElement('div');
             div.classList.add('user');
-            for (const key in arrayUser) {
-                let divChild = document.createElement('div');
-              function rec(valueObj, keyObj,  divChildren, divParent){
-                  if(typeof valueObj === 'object'){
-                      divChildren.innerText =`${keyObj}`;
-                      for (const argumentsKey in valueObj) {
-                          debugger;
-                          let divRecChild = document.createElement('div');
-                          divChildren.appendChild(divRecChild);
-                          if(typeof valueObj[argumentsKey] ==='object' ){
-                              console.log(divRecChild);
-                            rec(valueObj[argumentsKey],argumentsKey,divRecChild, divChildren);
-                          }
-                          else {
-                              divRecChild.innerText = `${argumentsKey}: ${valueObj[argumentsKey]}`;
-                          }
-                      }
-                  }
-                  else {
-                      divChildren.innerText = `${key}: ${arrayUser[key]}`;
-                  }
-                  divParent.appendChild(divChildren);
-              }
-              rec(arrayUser[key], key, divChild, div);
+
+            function refRec(obj, divParent) {
+                for (const key in obj) {
+                    if (key !== 'id') {
+                        let divChildren = document.createElement('div');
+                        if (typeof obj[key] === 'object') {
+                            divChildren.innerText = `${key}`;
+                            refRec(obj[key], divChildren);
+                        } else {
+                            divChildren.innerText = `${key}: ${obj[key]}`;
+                        }
+                        divParent.appendChild(divChildren);
+                    }
+                }
             }
+
+            refRec(arrayUser, div);
             let btn = document.createElement('button');
             btn.innerText = "Add posts of user";
             div.appendChild(btn);
             divContainer.appendChild(div);
-
-            btn.onclick = function (){
+            btn.onclick = function () {
                 fetch(`https://jsonplaceholder.typicode.com/posts?userId=${arrayUser.id}`)
                     .then(response => response.json())
-                    .then(arrayPosts=> {
-                        for (const arrayPost of arrayPosts){
+                    .then(arrayPosts => {
+                        for (const arrayPost of arrayPosts) {
                             let divPost = document.createElement('div');
                             let h2 = document.createElement('h2');
                             let p = document.createElement('p');
@@ -54,12 +45,12 @@ fetch('https://jsonplaceholder.typicode.com/users')
                             btn2.innerText = "Add comments of post";
                             h2.innerText = arrayPost.title;
                             p.innerText = arrayPost.body;
-                            divPost.append(h2,p,btn2);
+                            divPost.append(h2, p, btn2);
                             div.appendChild(divPost);
-                            btn2.onclick = function (){
+                            btn2.onclick = function () {
                                 fetch(`https://jsonplaceholder.typicode.com/comments?postId=${arrayPost.id}`)
                                     .then(response => response.json())
-                                    .then(arrayComments=> {
+                                    .then(arrayComments => {
                                         for (const arrayComment of arrayComments) {
                                             let divCom = document.createElement('div');
                                             let h2 = document.createElement('h2');

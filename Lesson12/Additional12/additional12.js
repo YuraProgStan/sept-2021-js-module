6e87b4dafd7968980a7d1b4b0decd67a97570712
YuraProgStan <yurstan@gmail.com>
2021-12-12 10:10:50 +0200

    Lesson12. all

--- Lesson12/Additional12/additional12.js
+++ Lesson12/Additional12/additional12.js
@@ -9,7 +9,7 @@ divContainer.classList.add('container');
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(arrayUsers => {
-        for (const arrayUser of arrayUsers) {
+        for (const objUser of arrayUsers) {
             let div = document.createElement('div');
             div.classList.add('user');
 
@@ -27,38 +27,37 @@ fetch('https://jsonplaceholder.typicode.com/users')
                     }
                 }
             }
-
-            refRec(arrayUser, div);
+            refRec(objUser, div);
             let btn = document.createElement('button');
             btn.innerText = "Add posts of user";
             div.appendChild(btn);
             divContainer.appendChild(div);
             btn.onclick = function () {
-                fetch(`https://jsonplaceholder.typicode.com/posts?userId=${arrayUser.id}`)
+                fetch(`https://jsonplaceholder.typicode.com/posts?userId=${objUser.id}`)
                     .then(response => response.json())
                     .then(arrayPosts => {
-                        for (const arrayPost of arrayPosts) {
+                        for (const objPost of arrayPosts) {
                             let divPost = document.createElement('div');
                             let h2 = document.createElement('h2');
                             let p = document.createElement('p');
                             let btn2 = document.createElement('button');
                             btn2.innerText = "Add comments of post";
-                            h2.innerText = arrayPost.title;
-                            p.innerText = arrayPost.body;
+                            h2.innerText = objPost.title;
+                            p.innerText = objPost.body;
                             divPost.append(h2, p, btn2);
                             div.appendChild(divPost);
                             btn2.onclick = function () {
-                                fetch(`https://jsonplaceholder.typicode.com/comments?postId=${arrayPost.id}`)
+                                fetch(`https://jsonplaceholder.typicode.com/comments?postId=${objPost.id}`)
                                     .then(response => response.json())
                                     .then(arrayComments => {
-                                        for (const arrayComment of arrayComments) {
+                                        for (const objComment of arrayComments) {
                                             let divCom = document.createElement('div');
                                             let h2 = document.createElement('h2');
                                             let p1 = document.createElement('p');
                                             let p2 = p1.cloneNode(true);
-                                            h2.innerText = arrayComment.name;
-                                            p1.innerText = arrayComment.email;
-                                            p2.innerText = arrayComment.body;
+                                            h2.innerText = objComment.name;
+                                            p1.innerText = objComment.email;
+                                            p2.innerText = objComment.body;
                                             divCom.append(h2, p1, p2);
                                             divPost.appendChild(divCom);
                                         }

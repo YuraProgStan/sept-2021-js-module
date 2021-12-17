4230824d4ab75391b5824dbfa6708aa81617d04c
YuraProgStan <yurstan@gmail.com>
2021-12-17 19:09:21 +0200

    Lesson13. Additional13

---
+++ Lesson13/Additional13/additional13.js
@@ -0,0 +1,39 @@
+/*
+- Імітуємо наповнення інтернет магазину товарами :
+    Створити форму з наступними полями :
+    - назва товару
+- кількість товару
+- ціна товару
+- картинка товару (посилання з інтернету)
+Зберігати товари в масив в локалсорадж. При збережені товару з форми, action не повинно відбуватись (preventDefault)
+створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на сторінці всі товари.
+На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
+    До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар*/
+let a = document.createElement('a');
+localStorage.clear();
+a.href = 'list.html';
+a.target= '_blank';
+a.innerText = 'Page product';
+document.getElementById('aDiv').appendChild(a);
+let form = document.forms[0];
+form.onsubmit = function (e) {
+    e.preventDefault();
+    let nameProduct = form.nameproduct.value;
+    let countProduct = form.countproduct.value;
+    let priceProduct = form.priceproduct.value;
+    let urlImage = form.urlimage.value;
+    let obj = {};
+    if (nameProduct && countProduct && countProduct && priceProduct && urlImage) {
+        obj.name = nameProduct;
+        obj.count = countProduct;
+        obj.price = priceProduct;
+        obj.url = urlImage;
+    }
+    let item = 'Products';
+    let saveProducts = (array) => {
+        let newProducts = JSON.parse(localStorage.getItem(item)) || [];
+        newProducts.push(array);
+        localStorage.setItem(item,JSON.stringify(newProducts) )
+    }
+    saveProducts(obj);
+}
---
+++ Lesson13/Additional13/list.js
@@ -0,0 +1,49 @@
+let products = JSON.parse(localStorage.getItem('Products'));
+let divProducts = document.createElement('div');
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+for (const product of products) {
+    let div = document.createElement('div');
+    div.classList.add('product');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let img = document.createElement('img');
+    let btn = document.createElement('button');
+    btn.innerText = 'Delete this product from the bin';
+    let divButton = document.createElement('div');
+    divButton.appendChild(btn);
+    h2.innerText = `${capitalizeFirstLetter(Object.keys(product)[0])}  ${product.name}`;
+    p1.innerText = `${capitalizeFirstLetter(Object.keys(product)[1])}  ${product.count}`;
+    p2.innerText = `${capitalizeFirstLetter(Object.keys(product)[2])}  ${product.price}`;
+    img.src = product.url;
+    div.append(h2, p1, p2, img, divButton);
+    divProducts.appendChild(div);
+    let item = 'Products';
+    btn.addEventListener('click', function () {
+        let products = JSON.parse(localStorage.getItem('Products'));
+        products.forEach((value, index, arr) => {
+            console.log('value', value.name);
+            console.log('product', product.name);
+                arr.splice(index, 1);
+                localStorage.removeItem(item);
+                localStorage.setItem(item, JSON.stringify(arr));
+                div.innerText = "";
+                location.reload();
+        })
+    })
+}
+let divBtnAll = document.createElement('div');
+let btnAll = document.createElement('button');
+btnAll.innerText = 'Delete all products from the bin';
+btnAll.classList.add('btn-all')
+divBtnAll.appendChild(btnAll);
+document.getElementsByClassName('container')[0].append(divProducts, divBtnAll);
+btnAll.addEventListener('click', function () {
+    localStorage.removeItem('Products');
+    let container = document.getElementsByClassName('container');
+    container.innerHTML = "";
+    location.reload();
+})
+
